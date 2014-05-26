bc381dc0acbfb643404deaf64a43b281ef08730c
Chris Hooker <hookercj@gmail.com>
2014-05-26 13:33:51 -0400

    next

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ var dice = {
 	"green": ["S", "S", "SS", "A", "A", "SA", "AA", ""],
 	"yellow": ["S", "S", "SS", "SS", "A", "SA", "SA", "SA", "AA", "AA", "*", ""],
 	"purple": ["F", "FF", "T", "T", "T", "TT", "FT", ""],
-	"red": ["F", "F", "FF", "FF", "T", "T", "FT", "FT", "TT", "TT", "X", ""]
+	"red": ["F", "F", "FF", "FF", "T", "T", "FT", "FT", "TT", "TT", "X", ""],
 	"blue": ["S", "SA", "AA", "A", "", ""],
-	"black": ["F", "F", "T", "T", "", ""],
+	"black": ["F", "F", "T", "T", "", ""]
 	};
