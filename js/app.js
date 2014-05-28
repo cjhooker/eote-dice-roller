96d4669772ff1e163af7f82a11c277615db97bb3
Chris Hooker <hookercj@gmail.com>
2014-05-27 22:28:24 -0400

    next

--- js/app.js
+++ js/app.js
@@ -43,13 +43,14 @@ function roll() {
 		messages = JSON.parse(gapi.hangout.data.getValue('messages'));
 	}
 	
-	var diceRolled = "";
-	var rollResult = "";
+	var diceRolled = new Array();
+	var diceResults = new Array();
+	
 	for (var color in dice) {
 		var qty = document.getElementById("qty" + color).value;
 		for (var i=0; i < qty; i++) {
-			rollResult += getRoll(dice[color]);
-			diceRolled += color.substring(0, 0);
+			diceRolled.push(color.substring(0, 0));
+			diceResults.push(getRoll(dice[color]));
 		}
 	}
 	
@@ -57,12 +58,44 @@ function roll() {
 	message.type = "roll";
 	message.participantId = gapi.hangout.getLocalParticipant().id;
 	message.data = new Object();
-	message.data.dice = diceRolled;
-	// message.data.quantity = 1;
-	message.data.result = rollResult;
+	message.data.diceRolled = diceRolled;
+	message.data.diceResults = diceResults;
+	message.data.overallResult = getOverallResult(diceResults);
 	messages.push(message);
 	
 	gapi.hangout.data.setValue("messages", JSON.stringify(messages));
+}
+
+function getOverallResult(diceResults) {
+	var result = "";
+	var allResults = diceResults.join("");
+	var successes = (allResults.match(/S/g) || []).length;
+	var failures = (allResults.match(/F/g) || []).length;
+	var advantages (allResults.match(/A/g) || []).length;
+	var threats = (allResults.match(/T/g) || []).length;
+	var triumphs (allResults.match(/\*/g) || []).length;
+	var despairs = (allResults.match(/X/g) || []).length;
+	
+	if (successes > failures) {
+		successes = successes - failures;
+	}
+
+	if (threats > advantages) {
+		threats = threats - advantages;
+		advantages = 0;
+	} else {
+		advantages = advantages - threats;
+		threats = 0;
+	}
+	
+	// Array(n).join(char) will produce a string of n-1 chars
+	result += Array(successes + 1).join("S");
+	result += Array(advantages + 1).join("A");
+	result += Array(threats + 1).join("T");
+	result += Array(triumphs + 1).join("*");
+	result += Array(despairs + 1).join("X");
+	
+	return result;
 }
 
 function getRoll(die) {
@@ -79,7 +112,9 @@ function displayMessage(message) {
 	
 	if (message.type = "roll") {
 		output += gapi.hangout.getParticipantById(message.participantId).person.displayName + ": ";
-		output += message.data.result + "<br/>";
+		output += message.data.diceRolled.join("") + "->";
+		output += message.data.diceResults.join("") + "->";
+		output += message.data.overallResults + "<br/>";
 	} else if (message.type = "html") {
 		output += message.data.html;
 	}
