1751ed1548968bc7b3a4b966da60718962a5048b
Chris Hooker <hookercj@gmail.com>
2014-06-21 20:40:42 -0400

    Moved data out of data.js to relevant angular objects.

--- js/directives/message-display.js
+++ js/directives/message-display.js
@@ -3,34 +3,22 @@
         restrict: 'E',
         templateUrl: "message-display.html",
         scope: {
-            //person: '@person',
             message: '=message'
         },
         link: function ($scope, element, attrs) {
-            var message = $scope.message;
-            $scope.person = gapi.hangout.getParticipantById(message.participantId).person;
-            //function getMessageHtml(message) {
-            var results = "";
-            var person = gapi.hangout.getParticipantById(message.participantId).person;
 
-            $scope.symbols = symbols;
+            $scope.person = gapi.hangout.getParticipantById($scope.message.participantId).person;
 
-            //switch (message.type) {
-            //    case "roll":
-            //        var results = "<div class='dice-results'>";
-            //        for (var i = 0; i < message.data.diceResults.length; i++) {
-            //            results += getDieImageHtml(message.data.diceResults[i], message.data.diceRolled[i]);
-            //        }
-            //        results += "</div><div class='overall-results'>";
-            //        results += getImagesHtml(message.data.overallResult) + "</div>";
-            //        break;
-            //    case "html":
-            //        results = message.data.html;
-            //        break;
-            //}
-
-            $scope.results = results;
-            //}
+            $scope.symbols = {
+                "S": "success.png",
+                "F": "failure.png",
+                "A": "advantage.png",
+                "T": "threat.png",
+                "X": "triumph.png",
+                "D": "despair.png",
+                "B": "black-circle.png",
+                "W": "white-circle.png"
+            }
         }
     };
 }]);
